2fe0fa6d22bc1d2bc036610028c412c69dd87442
mrrezaahmadi <mrrezaahmadi>
2020-06-21 18:26:35 +0430

    add functionality to add and remove buttons

--- script.js
+++ script.js
@@ -25,17 +25,41 @@ const foods = [{
     id: 4
 }];
 
+const add = (id) => {
+    const addedItem = foods.find(food => {
+        return food.id === id
+    })
+
+    addedItem.amount += 1
+    addedItem.value = addedItem.amount * addedItem.price
+    renderer(foods)
+}
+const remove = (id) => {
+    const removedItem = foods.find(food => {
+        return food.id === id
+    })
+
+    if (removedItem.amount > 0) {
+        removedItem.amount -= 1
+        removedItem.value = removedItem.amount * removedItem.price
+    }
+    renderer(foods)
+}
+
 
 const renderer = (foods) => {
+    $('.main').html('')
     foods.forEach(food => {
         $('.main').append(`
             <h3>${food.name}</h3>
             <p>Price: ${food.price}</p>
             <p>Amount: ${food.amount}</p>
             <p>Value: ${food.value}</p>
-            <button id="add" > + </button>
-            <button id="remove" > - </button>
+            <button onClick="add(${food.id})" id="addBtn" > + </button>
+            <button onClick="remove(${food.id})" id="removeBtn" > - </button>
         `)
+
+
     });
 }
 
